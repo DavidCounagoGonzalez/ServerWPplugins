8bfa95ca7e416aee11c8a0dc4c45e8b13823628d
David <dcounagogonzalez@danielcastelao.org>
2022-11-22 11:01:25 +0100

    Tabla creada

--- html/wp-content/plugins/Tabla/UsoBD.php
+++ html/wp-content/plugins/Tabla/UsoBD.php
@@ -1,15 +1,13 @@
 <?php
 
 /*
-Plugin Name: Plugin MalsonanteDB
-Plugin URI: http://wordpress.org/plugins/hello-dolly/
-Description: Busca en la página palabras malsonantes y las cambia por otras gracias a una BD
-Author: DAM21
-Version: 1.7.2
-Author URI: http://ma.tt/
+Plugin Name: MalsonantesDB
+Plugin URI: http://www.danielcastelao.org/
+Description: Experimentación de varias técnicas para hacer un plugin
+Version: 1.0
 */
 
-function renym_wordpress_typo_fix( $text ) {
+function myplugin_update_db_check() {
     // Objeto global del WordPress para trabajar con la BD
     global $wpdb;
 
@@ -19,45 +17,21 @@ function renym_wordpress_typo_fix( $text ) {
     // le añado el prefijo a la tabla
     $table_name = $wpdb->prefix . 'dam';
 
-    // recogemos todos los datos de la tabla
-    // los metemos en un array asociativo, en vez de indices numericos,
-    // los indices son los nombres de las columnas de la tabla
-    $resultado = $wpdb->get_results("SELECT * FROM " . $table_name, ARRAY_A);
-
-    // recorremos el resultado
-    foreach($resultado as $fila)
-    {
-        // mostramos el resultado en los logs
-        error_log("Recorremos resultado: " . $fila['time']);
-    }
-
-    return str_replace( 'WordPress', 'WordPressDAM', $text );
-}
-
-/**
- * Añadimos la función renym_wordpress_typo_fix al filtro 'the_content'
- * Se ejecutará cada vez que se cargue un post
- */
-add_filter( 'the_content', 'renym_wordpress_typo_fix' );
-
-function plugin_db_update() {
-    global $wpdb;
-
-    $charset_collate = $wpdb->get_charset_collate();
-
-    $table_name = $wpdb->prefix . 'dam';
-
-    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
-    id mediumint(9) NOT NULL AUTO_INCREMENT,
-    time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-    name tinytext NOT NULL,
-    text text NOT NULL,
-    url varchar(55) DEFAULT '' NOT NULL,
-    PRIMARY KEY (id)
+    // creamos la sentencia sql
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        name tinytext NOT NULL,
+        text text NOT NULL,
+        PRIMARY KEY (id)
     ) $charset_collate;";
 
+    // libreria que necesito para usar la funcion dbDelta
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
     dbDelta( $sql );
+
+
+    // insertamos valores
 
     $name='Pepe';
     $text='Hola Mundo';
@@ -72,6 +46,9 @@ function plugin_db_update() {
     );
 
     error_log("Plugin DAM insert: " . $result);
-
 }
-add_action( 'plugins_loaded', 'plugin_db_update' );
+
+/**
+ * Ejecuta 'myplugin_update_db_check', cuando el plugin se carga
+ */
+add_action( 'plugins_loaded', 'myplugin_update_db_check' );
